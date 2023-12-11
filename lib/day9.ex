6826665d1912fe725fadc3e722c0f3a3aa93069c
Chris Doggett <doggettc@gmail.com>
2023-12-11 17:26:26 -0600

    Finished day 9

--- lib/day9.ex
+++ lib/day9.ex
@@ -1,8 +1,51 @@
 defmodule Day9 do
-  def part_one(_input) do
+  def part_one(input) do
+    input
+    |> Enum.map(&extrapolate/1)
+    |> Enum.sum()
   end
 
-  def part_two(_input) do
+  def part_two(input) do
+    input
+    |> Enum.map(&extrapolate_backwards/1)
+    |> Enum.sum()
   end
 
+  # Part 1
+  defp extrapolate(line) do
+    line
+    |> Common.string_to_integers()
+    |> Enum.reverse()
+    |> make_subtractions([])
+    |> make_extrapolations()
+  end
+
+  defp make_subtractions(list, acc) do
+    next_list =
+      list
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(fn [a, b] -> a - b end)
+
+    case Enum.uniq(next_list) do
+      [0] ->
+        [list | acc]
+
+      _ ->
+        make_subtractions(next_list, [list | acc])
+    end
+  end
+
+  defp make_extrapolations([[result | _]]), do: result
+
+  defp make_extrapolations([[inc | _], [acc | _] = next | rest]) do
+    make_extrapolations([[acc + inc | next] | rest])
+  end
+
+  # Day 2
+  defp extrapolate_backwards(line) do
+    line
+    |> Common.string_to_integers()
+    |> make_subtractions([])
+    |> make_extrapolations()
+  end
 end
--- test/day9_test.exs
+++ test/day9_test.exs
@@ -3,25 +3,25 @@ defmodule Day9Test do
   import AdventOfCode2023.Test.Common
 
   test "part one example" do
-    assert nil ==
+    assert 114 ==
              load_input(9, "example")
              |> Day9.part_one()
   end
 
   test "part one real" do
-    assert nil ==
+    assert 2_038_472_161 ==
              load_input(9, "real")
              |> Day9.part_one()
   end
 
   test "part two example" do
-    assert nil ==
+    assert 2 ==
              load_input(9, "example")
              |> Day9.part_two()
   end
 
   test "part two real" do
-    assert nil ==
+    assert 1091 ==
              load_input(9, "real")
              |> Day9.part_two()
   end
